243a03adbc69217f4a2aa5c79e1eeaf8c2e7bbce
mac <mac@macs-air.lan>
2019-11-27 21:30:06 -0500

    added start and end decorators

--- bvr/bvr_end.py
+++ bvr/bvr_end.py
@@ -1,4 +1,22 @@
 
 
-def bvr_end():
-    pass
+def bvr_end(arg=None):
+    def bvr_end_decorator(func):
+        def bvr_end_wrapper(*args, **kwargs):
+            msg = ("ENDED | "
+                   "FUNCTION: {} | "
+                   "ARGS: {} | "
+                   "KWARGS: {} ").format(func.__name__,
+                                         args,
+                                         kwargs)
+
+            print(msg)
+
+            return_value = func(*args, **kwargs)
+            return return_value
+        return bvr_end_wrapper
+
+    if callable(arg):
+        return bvr_end_decorator(arg)
+    else:
+        return bvr_end_decorator
--- bvr/bvr_start.py
+++ bvr/bvr_start.py
@@ -1,4 +1,22 @@
 
 
-def bvr_start():
-    pass
+def bvr_start(arg=None):
+    def bvr_start_decorator(func):
+        def bvr_start_wrapper(*args, **kwargs):
+            msg = ("STARTED | "
+                   "FUNCTION: {} | "
+                   "ARGS: {} | "
+                   "KWARGS: {} ").format(func.__name__,
+                                         args,
+                                         kwargs)
+
+            print(msg)
+
+            return_value = func(*args, **kwargs)
+            return return_value
+        return bvr_start_wrapper
+
+    if callable(arg):
+        return bvr_start_decorator(arg)
+    else:
+        return bvr_start_decorator
